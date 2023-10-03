020427a2f0f5e8bb79cb9eb4c902979d03ebc955
Leonid Sukhinin <leonsukh@gmail.com>
2023-10-03 23:51:09 +0300

    upd

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"net/http"
+    "net/http"
 )
 
 func main() {
-	db, err := InitDB("./clicks.db")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+    db, err := InitDB("./clicks.db")
+    if err != nil {
+        panic(err)
+    }
+    defer db.Close()
 
-	http.HandleFunc("/", RootHandler(db))
-	http.ListenAndServe(":80", nil)
+    http.HandleFunc("/", RootHandler(db))
+
+    // Specify the paths to your SSL certificate and private key files.
+    certFile := "/path/to/your/ssl-certificate.pem"
+    keyFile := "/path/to/your/private-key.pem"
+
+    // Start an HTTPS server using your SSL certificate and private key.
+    err = http.ListenAndServeTLS(":443", certFile, keyFile, nil)
+    if err != nil {
+        panic(err)
+    }
 }
