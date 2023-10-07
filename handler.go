4779ca2b65aa5a38947c47e3d81879051d0a0a73
Leonid Sukhinin <leonsukh@gmail.com>
2023-10-07 21:46:58 +0300

    upd

--- handler.go
+++ handler.go
@@ -6,12 +6,6 @@ import (
 	"time"
 )
 
-// Define a map of redirects based on the "r" parameter
-var redirects = map[string]string{
-	"kj_bennet": "https://fans.ly/subscriptions/giftcode/NTY3MDk5Nzg3NDM4OTkzNDEwOjE6MTowYjMwNmY1NzA3",
-	// Add more mappings as needed
-}
-
 func RootHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		userAgent := r.Header.Get("User-Agent")
@@ -24,12 +18,10 @@ func RootHandler(db *sql.DB) http.HandlerFunc {
 		cookie, err := r.Cookie("visited")
 
 		if err != nil {
-			// Check if the "r" parameter is in the redirects map
-			if redirectURL, ok := redirects[referral]; ok {
-				http.Redirect(w, r, redirectURL, http.StatusSeeOther)
-			} else {
-				// Redirect to a default URL if "r" is not found in the map
-				http.Redirect(w, r, "https://fansly.com/VikiMinelli/posts", http.StatusSeeOther)
+			err = UpdateClicks(db, referral)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
 
 			expire := time.Now().AddDate(1, 0, 0)
@@ -39,5 +31,19 @@ func RootHandler(db *sql.DB) http.HandlerFunc {
 				Expires: expire,
 			})
 		}
+
+		// Define a map of redirects based on the "r" parameter
+		redirects := map[string]string{
+			"kj_bennet": "https://fans.ly/subscriptions/giftcode/NTY3MDk5Nzg3NDM4OTkzNDEwOjE6MTowYjMwNmY1NzA3",
+		}
+
+		// Check if the "r" parameter is in the redirects map
+		if redirectURL, ok := redirects[referral]; ok {
+			http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+			return
+		}
+
+		// Default redirect to Google.com
+		http.Redirect(w, r, "https://google.com", http.StatusSeeOther)
 	}
 }
