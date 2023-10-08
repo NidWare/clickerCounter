771da860ce8ca356eef6e2e91229aef9db2c124c
Leonid Sukhinin <leonsukh@gmail.com>
2023-10-08 19:44:48 +0300

    upd

--- handler.go
+++ handler.go
@@ -16,39 +16,45 @@ func RootHandler(db *sql.DB) http.HandlerFunc {
 		}
 
 		referral := r.URL.Query().Get("r")
-		cookie, err := r.Cookie("visited")
 
-		redirectURL := "https://fansly.com/VikiMinelli/posts" // Change this to your default redirect URL
+		redirectURL := "https://fansly.com/VikiMinelli/posts"
+
 		if referral != "" {
 			redirects, err := GetRedirects(db)
-
 			if err != nil {
 				fmt.Println("Error caught while getting redirects")
 			}
-			// Check if the "r" parameter is in the redirects map
 			if mappedURL, ok := redirects[referral]; ok {
 				redirectURL = mappedURL
 			}
 		}
 
-		// Redirect to the determined URL
 		http.Redirect(w, r, redirectURL, http.StatusSeeOther)
 
-		if err != nil || cookie.Value != "true" {
-			// Update clicks only if the user hasn't visited before
-			err = UpdateClicks(db, referral)
-			if err != nil {
+		cookie, err := r.Cookie("visited")
+
+		if err != nil {
+			if err == http.ErrNoCookie {
+				err = UpdateClicks(db, referral)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+
+				expire := time.Now().AddDate(1, 0, 0)
+				http.SetCookie(w, &http.Cookie{
+					Name:    "visited",
+					Value:   "true",
+					Expires: expire,
+				})
+			} else {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-
-			// Set the visited cookie to indicate that the user has visited
-			expire := time.Now().AddDate(1, 0, 0)
-			http.SetCookie(w, &http.Cookie{
-				Name:    "visited",
-				Value:   "true",
-				Expires: expire,
-			})
+		} else {
+			if cookie.Value != "true" {
+				fmt.Println("Strange cookies")
+			}
 		}
 	}
 }
